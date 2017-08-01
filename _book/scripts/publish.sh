35b2e71e3bfbee7c36d084b0e37c690fa0453d52
Evan Cole <evan.t.cole@gmail.com>
2017-08-01 18:44:44 +0200

    publishing

---
+++ _book/scripts/publish.sh
@@ -0,0 +1,25 @@
+# # checkout to the gh-pages branch
+# git checkout gh-pages
+# # pull the latest updates
+# git pull janke gh-pages --rebase
+# # install the plugins and build the static site
+# gitbook install && gitbook build
+# # copy the static site files into the current directory.
+# cp -R _book/* .
+# # remove 'node_modules' and '_book' directory
+# git clean -fx node_modules
+# git clean -fx _book
+# # add all files
+# git add .
+# # commit
+# git commit -a -m "update path"
+# # push to the origin
+# git push janke gh-pages
+# # checkout to the master branch
+# git checkout master
+
+# rewriting my own
+
+git add .
+git commit -m 'publishing'
+cp -r ./_book ../book-holder
