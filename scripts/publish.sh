2ed2200cded68bddb796fe8afee33fe3c1faa263
colevandersWands <evan.t.cole@gmail.com>
2017-09-16 02:19:31 +0200

    publishing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,12 +6,20 @@ git checkout master
 gitbook build
 # copy it out of the way for branch change
 # f to overwirte last publication
+<<<<<<< HEAD
+cp -rf ./_book ../book-holder
+=======
 cp -rf ./_book/. ../book-holder/ 
+>>>>>>> 527fa16c5ce892d27f6c2517b2029072b47a98a4
 # commit recent build
 git add .
 git commit -m 'publishing'
 # push newest source
+<<<<<<< HEAD
+git push origin master
+=======
 # git push origin master
+>>>>>>> 527fa16c5ce892d27f6c2517b2029072b47a98a4
 # switch to gh-pages branch
 git checkout gh-pages
 # copy the new book in
@@ -30,6 +38,14 @@ git checkout master
 
 
 ################################################
+<<<<<<< HEAD
+#  this script requires a file structure like this:
+# 
+#	 ** a host directory, empty but for the cloned repo **
+# 	|
+# 	|- pathbook -> the cloned repo
+# 		|- the book md (when not on gh-pages)
+=======
 #  Recommended file structure for using this script
 # 
 #	| ## documents, desktop, whatevs ##
@@ -37,3 +53,4 @@ git checkout master
 # 	|- gitbooking ## -> contains only these two folders, ever
 # 		|- gitbook ## -> SUMMARY, README, markdowns, config, ...
 #		|- book-holder ## -> to stash built gitbook while changing branches
+>>>>>>> 527fa16c5ce892d27f6c2517b2029072b47a98a4
